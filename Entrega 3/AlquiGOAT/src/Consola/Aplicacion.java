29897932bb7b4c1cf8af566ab44167aecac69324
Nicolás Arango <123480637+Narangor@users.noreply.github.com>
2023-10-13 10:42:24 -0500

    main

--- Entrega 3/AlquiGOAT/src/Consola/Aplicacion.java
+++ Entrega 3/AlquiGOAT/src/Consola/Aplicacion.java
@@ -1,5 +1,25 @@
 package Consola;
 
 public class Aplicacion {
+	
+	private int opcionSeleccionada;
+	
+	public static void iniciarSesion() {
+		
+	}
+	
+	public static void mostrarMenu() {
+		
+	}
+	
+	public static void ejecutarOpcion() {
+		
+	}
+	
+	public static void main(String [] args) {
+		
+		
+		
+	}
 
 }
