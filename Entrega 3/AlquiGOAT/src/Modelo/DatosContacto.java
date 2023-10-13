45b13b8a80c6ce54187d5c5acaafd7746a675b81
Nicolás Arango <123480637+Narangor@users.noreply.github.com>
2023-10-13 11:31:10 -0500

    data datos

--- Entrega 3/AlquiGOAT/src/Modelo/DatosContacto.java
+++ Entrega 3/AlquiGOAT/src/Modelo/DatosContacto.java
@@ -2,4 +2,11 @@ package Modelo;
 
 public class DatosContacto {
 
+    private String numeroCelular;
+    private String correo;
+    private String paisResidencia;
+    private String ciudadResidencia;
+    private String direccionResidencia;
+    private String codigoPostal;
+
 }
