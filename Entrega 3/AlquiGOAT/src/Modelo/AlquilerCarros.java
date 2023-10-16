fe43d8c684be37085e090fe187cb3ac05d64a343
catalvarez22 <123499204+catalvarez22@users.noreply.github.com>
2023-10-16 13:15:59 -0500

    cata4

--- Entrega 3/AlquiGOAT/src/Modelo/AlquilerCarros.java
+++ Entrega 3/AlquiGOAT/src/Modelo/AlquilerCarros.java
@@ -12,6 +12,7 @@ public class AlquilerCarros {
     private HashMap<String, Usuario> usuarios;
     private HashMap<String, Categorias> categorias;
     private CargarDatos cargarDatos;
+    private GuardarDatos guardarDatos;
 
 
     public String revisarCuenta(String usuario, String password) {
@@ -35,10 +36,28 @@ public class AlquilerCarros {
         sedes = cargarDatos.cargarSedes() ;
         carros = cargarDatos.cargarVehiculos(sedes, categorias);
         usuarios = cargarDatos.cargarUsuarios(sedes);
+        guardarDatos = new GuardarDatos(todosTrabajadores, todosClientes, todosCarros, todasSedes);
         
     }
     
-    
+   public void crearCliente(String nombreUsuario, String login, String password, String fechaNacimiento,
+			String nacionalidad, File imagenDocumento, String numeroCelular, String correo, String paisResidencia,
+			String ciudadResidencia, String direccionResidencia, String codigoPostal, String numeroID,
+			String paisExpedicion, String fechaCaducidadL, File imagenLicencia, String numeroTarjeta,
+			String codigoTarjeta, String fechaCaducidadT, String tipo){
+		Licencia datosLicencia = new Licencia(numeroID, paisExpedicion, fechaCaducidadL, imagenLicencia);
+		Tarjeta datosTarjeta = new Tarjeta(numeroTarjeta, codigoTarjeta, fechaCaducidadT, tipo);
+		DatosContacto dataContact = new DatosContacto(numeroCelular, correo, paisResidencia, ciudadResidencia,
+				direccionResidencia, codigoPostal);
+		Cliente cliente = new Cliente(nombreUsuario, login, password, fechaNacimiento, nacionalidad, imagenDocumento,
+				dataContact, datosLicencia, datosTarjeta);
+		usuarios.put(login, cliente);
+		guardarDatos.addCliente(nombreUsuario, login, password, fechaNacimiento,
+			nacionalidad, imagenDocumento, numeroCelular, correo, paisResidencia,
+			ciudadResidencia, direccionResidencia, codigoPostal, numeroID,
+			paisExpedicion, fechaCaducidadL, imagenLicencia, numeroTarjeta,
+			codigoTarjeta, fechaCaducidadT, tipo);
+			}
     
 
 }
